2e6d52d769ee5dcffa96f7e707ef09bad274a7a6
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-27 23:13:03 +0530

    created an object of MyBinarySearchTree

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,11 +7,14 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 import java.util.Scanner;
 
 public class MyMain {
     public static void main(String[] args){
         Scanner sc = new Scanner( System.in );
+        MyBinarySearchTree bst = new MyBinarySearchTree();
 
     }
 }
