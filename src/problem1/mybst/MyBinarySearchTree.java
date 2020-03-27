8233a35995b394bed38309ad3ccdd366f3d19c0f
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-27 23:21:48 +0530

    default constructor for creating an array list

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,19 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
+import java.util.ArrayList;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private TreeNode root;
+    private ArrayList<Integer> arrayList;
+
+    public MyBinarySearchTree(){
+        arrayList = new ArrayList<>();
+    }
+
+
 }
