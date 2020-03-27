57787a43815bba7fa2da0e17b53d3e2f4906667b
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-27 23:19:27 +0530

    getter setter method and toString method for node class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,5 +11,32 @@ public class TreeNode {
     private TreeNode leftNode;
     private TreeNode rightNode;
 
+    public int getData(){
+        return data;
+    }
 
+    public void setData(int data){
+        this.data = data;
+    }
+
+    public TreeNode getLeftNode(){
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode){
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode(){
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode){
+        this.rightNode = rightNode;
+    }
+
+    @Override
+    public String toString(){
+        return "data==>" + getData();
+    }
 }
