e0a69b108a4f40b7f09c6bd84e370f17174e9593
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-27 23:17:44 +0530

    created a variable data to hold the value and left and right node

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,6 +8,7 @@ package problem1.main;
 // executable class
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
 import java.util.Scanner;
 
@@ -18,6 +19,9 @@ public class MyMain {
         System.out.println( "Enter the number of nodes" );/*-->*/
         int n = sc.nextInt();
         while(n-- > 0) {
+            System.out.println( "Enter the data" );
+            int val = sc.nextInt();
+            TreeNode node = new TreeNode();
 
         }
     }
